a0190c40b3906f86487306b2ea551361952a98b2
luis@yo.es <luis@yo.es>
2015-11-19 12:26:47 +0100

    Terminado

---
+++ WebVehiculos/Controllers/TipoVehiculoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebVehiculos.Models;
+
+namespace WebVehiculos.Controllers
+{
+    public class TipoVehiculoController : Controller
+    {
+       VehiculosLuisEntities db=new VehiculosLuisEntities();
+        // GET: TipoVehiculo
+        public ActionResult Index()
+        {
+
+            return View(db.TipoVehiculo.ToList());
+        }
+
+        public ActionResult Alta()
+        {
+            return View(new TipoVehiculo());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Alta(TipoVehiculo model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TipoVehiculo.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+            return View(model);
+        }
+
+    }
+}
---
+++ WebVehiculos/Controllers/VehiculosController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebVehiculos.Models;
+
+namespace WebVehiculos.Controllers
+{
+    public class VehiculosController : Controller
+    {
+        VehiculosLuisEntities db=new VehiculosLuisEntities();
+        // GET: Vehiculos
+        public ActionResult Index(int id)
+        {
+            ViewBag.idTipo = id;
+
+            var data = db.Vehiculo.Where(o => o.idTipo == id);
+
+
+            return View(data);
+        }
+
+        public ActionResult Buscar(int idTipo,
+            int campo, String contenido)
+        {
+            var data = db.Vehiculo.Where(o => o.idTipo == idTipo);
+            if (campo == 1)
+            {
+                data = data.Where(o => o.matricula.Contains(contenido));
+            }
+            else
+            {
+                data = data.Where(o => o.marca.Contains(contenido));
+            }
+
+            return PartialView("_Listado", data.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Vehiculo model)
+        {
+            db.Vehiculo.Add(model);
+            db.SaveChanges();
+            return Json(model);
+        }
+
+    }
+}

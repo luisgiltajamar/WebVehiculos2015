bb8ed60ee6b62406b25cdc5302d003e39f5e1001
luis@yo.es <luis@yo.es>
2015-11-19 12:45:23 +0100

    Con routing

--- WebVehiculos/App_Start/RouteConfig.cs
+++ WebVehiculos/App_Start/RouteConfig.cs
@@ -14,6 +14,14 @@ namespace WebVehiculos
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name:"BuscarVehiculo",
+                url:"Vehiculos/Buscar/{idTipo}/{campo}/{contenido}",
+                defaults:new
+                {
+                    controller="Vehiculos",
+                    action="Buscar"
+                });
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
